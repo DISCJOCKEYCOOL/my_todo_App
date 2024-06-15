d6ce2a22f73ed60bd16786bc1ee5568fe30f7c23
DISCJOCKEYCOOL <146633419+DISCJOCKEYCOOL@users.noreply.github.com>
2024-06-15 18:22:27 +0100

    Update functions.py-2

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-FILEPATH = "/Users/DJcoding/Desktop/Python_Projects/web_app1/pythonProject/todos.txt"
+FILEPATH = "todos.txt"
 
-def get_todos(filepath="/Users/DJcoding/Desktop/Python_Projects/web_app1/pythonProject/todos.txt"):
+def get_todos(filepath="todos.txt"):
     """Read a text file
     and return the list of text files"""
 
@@ -11,7 +11,7 @@ def get_todos(filepath="/Users/DJcoding/Desktop/Python_Projects/web_app1/pythonP
 
 def write_todos(todos_arg, filepath="todos.txt"):
     """Write a todos item list in a text file"""
-    with open("/Users/DJcoding/Desktop/Python_Projects/web_app1/pythonProject/todos.txt", "w") as file_global:
+    with open("todos.txt", "w") as file_global:
         file_global.writelines(todos_arg)
 
 if __name__ == "__main__":
