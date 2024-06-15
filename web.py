60a3212f884f59cd14edd89da917daec08de0344
DISCJOCKEYCOOL <146633419+DISCJOCKEYCOOL@users.noreply.github.com>
2024-06-15 19:16:06 +0100

    Update web.py

--- web.py
+++ web.py
@@ -10,7 +10,7 @@ def add_todo():
 
 st.title("MY todo app")
 st.subheader("This is my todo app")
-st.write("This app is to increase <b>your</b> productivity"
+st.write("This app is to increase <b>your</b> productivity",
         unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
