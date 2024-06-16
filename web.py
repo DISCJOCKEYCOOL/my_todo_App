db5fc0be66c13f694cd34051e011a6dcb6b3d980
DISCJOCKEYCOOL <146633419+DISCJOCKEYCOOL@users.noreply.github.com>
2024-06-16 07:43:06 +0100

    Update web.py

--- web.py
+++ web.py
@@ -39,7 +39,7 @@ if st.button('Alka'):
 
 else:
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
